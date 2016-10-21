df46002e852e8bc32f2fd3d0cb1b8b9da32b4fa0
Marcin Grzejszczak <mgrzejszczak@pivotal.io>
2016-10-21 10:46:52 +0200

    Added missing Eureka impl

--- src/main/java/com/example/DemoApplication.java
+++ src/main/java/com/example/DemoApplication.java
@@ -2,10 +2,12 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.messaging.Source;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @EnableBinding(Source.class)
 public class DemoApplication {
 
